896d7cf9d582454218c75f302e5ecf94b2ff5d04
PennJ1 <penn.f.johnakin@outlook.com>
2019-02-13 23:44:18 -0500

    add existing file

---
+++ assets/game.js
@@ -0,0 +1,29 @@
+var computerChoices = Math.floor((Math.random() * 120) + 1);
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+}
+
+function myFunction() {
+    var button1 = document.getElementById(event)
+    button1.innerHTML = Math.floor((Math.random() * 12) + 1);
+    var userGuess = event.key;
+  }
+  
+function myFunction() {
+    var button2 = document.getElementById(event)
+    button2.innerHTML = Math.floor((Math.random() * 12) + 1);
+    var userGuess = event.key;
+ }
+
+ function myFunction() {
+    var button3 = document.getElementById(event)
+    button3.innerHTML = Math.floor((Math.random() * 12) + 1);
+    var userGuess = event.key;
+  }
+
+  function myFunction() {
+    var button4 = document.getElementById(event)
+    button4.innerHTML = Math.floor((Math.random() * 12) + 1);
+    var userGuess = event.key;
+  }
